40414168d1e8eaaac7bb5f6e23de7f71084c2714
Kammela15Manasa <100776386+Kammela15Manasa@users.noreply.github.com>
2022-03-19 19:53:00 +0530

    Add files via upload

---
+++ script_1.js
@@ -0,0 +1,41 @@
+console.log("Hello World")
+
+var a = ("Hi this is Kammela Manasa")
+console.log(a)
+
+var b =  2
+console.log(b)
+
+var c = 2.5 
+console.log(c)
+
+var d = true 
+console.log(d)
+
+var e = false
+console.log(e)
+
+
+//  Const & Let
+const priceA = 5;
+const priceB = 6;
+let total = priceA + priceB;
+console.log(total)
+
+// Area of Circle(pi*r*r)---------A
+// Circumference Of Circle--------B
+// Radius Of Circle---------------R
+
+var A = 0
+const pi=3.14
+var r=5
+A= r*r*pi
+B=2*pi*r
+R=(B/2*pi)
+console.log(A)
+console.log(B)
+console.log(R)
+
+
+
+ 
